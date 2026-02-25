e864179d2c11e879c0dceada98b4c2aabcdac957
Ben Potter <bp@benpot.com>
2026-02-25 11:38:19 -0500

    auto recompile .asy files on buffer save

--- nvim/init.lua
+++ nvim/init.lua
@@ -230,6 +230,14 @@ vim.api.nvim_create_autocmd('FileType', {
   end,
 })
 
+vim.api.nvim_create_autocmd('BufWritePost', {
+  pattern = '*.asy',
+  callback = function()
+    local file = vim.fn.expand '%:p'
+    vim.cmd('silent! !' .. 'asy ' .. file)
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
