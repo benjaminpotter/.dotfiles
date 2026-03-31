abaa5f454c360804515f3db9855a0356859f34b9
Ben Potter <bp@benpot.com>
2026-03-31 12:31:07 -0400

    reformat kickstart nvim to personal configuration

---
+++ nvim/lua/ben/init.lua
@@ -0,0 +1,60 @@
+require("ben.set")
+require("ben.remap")
+require("ben.init_lazy")
+
+-- TODO: Investigate augroups: why? what do they do?
+
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.hl.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
+-- Configuration for .asy files.
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'asy',
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.expandtab = true
+  end,
+})
+
+vim.api.nvim_create_autocmd('BufWritePost', {
+  pattern = 'asy',
+  callback = function()
+    local file = vim.fn.expand '%:p'
+    vim.cmd('silent! !' .. 'asy ' .. file)
+  end,
+})
+
+-- Configuration for .tex and similar.
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'tex',
+  callback = function()
+    vim.cmd 'filetype plugin on'
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.expandtab = true
+  end,
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'bib',
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.expandtab = true
+  end,
+})
