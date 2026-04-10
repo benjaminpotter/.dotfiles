855ad76bff31738b2dba7f0cabc834398176781c
Ben Potter <bp@benpot.com>
2026-04-10 13:03:10 -0400

    try fix for waybar; disable vimtex quickfix menu

--- nvim/lua/ben/lazy/init.lua
+++ nvim/lua/ben/lazy/init.lua
@@ -333,8 +333,8 @@ return {
     init = function()
       -- VimTeX configuration goes here, e.g.
       vim.g.vimtex_view_method = 'zathura'
-      vim.g.vimtex_complete_enabled = false
-      vim.g.vimtex_quickfix_autoclose_after_keystrokes = 1
+      vim.g.vimtex_complete_enabled = 0 
+      vim.g.vimtex_quickfix_enabled = 0
     end,
   },
 
